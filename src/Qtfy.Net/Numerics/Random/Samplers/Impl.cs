2df099be5590d01174f121dbc2ff374ed13510bb
quantifeye <quantifeye@outlook.com>
2021-12-07 13:44:12 -0500

    added unit tests and fixes for cholesky decomposition

--- src/Qtfy.Net/Numerics/Random/Samplers/Impl.cs
+++ src/Qtfy.Net/Numerics/Random/Samplers/Impl.cs
@@ -29,14 +29,12 @@ namespace Qtfy.Net.Numerics.Random.Samplers
         /// <exception cref="ArgumentException">
         /// If <paramref name="covarianceMatrix"/> is not zero indexed,
         /// if <paramref name="covarianceMatrix"/> is not symmetric,
-        /// if <paramref name="covarianceMatrix"/> is empty,
-        /// if <paramref name="covarianceMatrix"/> is not positive definite.
-        /// if <paramref name="covarianceMatrix"/> contain any non-finite values.
+        /// if <paramref name="covarianceMatrix"/> is not positive definite,
+        /// if <paramref name="covarianceMatrix"/> is not a valid covariance matrix.
         /// </exception>
         internal static double[] PackedCholeskyFactorCovarianceMatrix(double[,] covarianceMatrix)
         {
-            CheckDimensions(covarianceMatrix);
-            CheckCovarianceValues(covarianceMatrix);
+            AssertValidCovarianceMatrix(covarianceMatrix);
             return PackedCholeskyDecomposition(covarianceMatrix);
         }
 
@@ -51,52 +49,37 @@ namespace Qtfy.Net.Numerics.Random.Samplers
         /// The factored correlation matrix.
         /// </returns>
         /// <exception cref="ArgumentNullException">
-        /// If <paramref name="correlationMatrix"/> is null.
-        /// </exception>
-        /// <exception cref="ArgumentException">
         /// If <paramref name="correlationMatrix"/> is not zero indexed,
         /// if <paramref name="correlationMatrix"/> is not symmetric,
-        /// if <paramref name="correlationMatrix"/> is empty,
-        /// if <paramref name="correlationMatrix"/> is not positive definite.
-        /// if any value in <paramref name="correlationMatrix"/> is not a valid correlation.
-        /// if any value on the diagonal if <paramref name="correlationMatrix"/> is not equal to one.
+        /// if <paramref name="correlationMatrix"/> is not positive definite,
+        /// if <paramref name="correlationMatrix"/> is not a valid covariance matrix.
         /// </exception>
         internal static double[] PackedCholeskyFactorCorrelationMatrix(double[,] correlationMatrix)
         {
-            CheckDimensions(correlationMatrix);
-            CheckCorrelationValues(correlationMatrix);
+            AssertValidCorrelationMatrix(correlationMatrix);
             return PackedCholeskyDecomposition(correlationMatrix);
         }
 
-        /// <summary>
-        /// Performs the cholesky decomposition of the matrix, and returns it in packed  lower triangular form.
-        /// </summary>
-        /// <param name="matrix">
-        /// The matrix to factor.
-        /// </param>
-        /// <returns>
-        /// The row major lower triangular matrix data of the decomposition.
-        /// </returns>
         private static double[] PackedCholeskyDecomposition(double[,] matrix)
         {
             unsafe
             {
-                var order = matrix.GetLength(0);
-                var result = new double[order * (order + 1) / 2];
+                var rows = matrix.GetLength(0);
+                var result = new double[rows * (rows + 1) / 2];
                 fixed (double* m = matrix, r = result)
                 {
-                    PackedCholeskyDecompositionImpl(m, r, order);
+                    PackedCholeskyDecompositionImpl(m, r, rows);
                 }
 
                 return result;
             }
         }
 
-        private static unsafe void PackedCholeskyDecompositionImpl(double* matrix, double* result, int order)
+        private static unsafe void PackedCholeskyDecompositionImpl(double* matrix, double* result, nint order)
         {
-            nint i, j, k;
             double* outputRowJ, inputRow, outputRowI;
             double sum;
+            nint i, j;
 
             outputRowI = result;
             for (i = 0; i < order; i++)
@@ -107,44 +90,34 @@ namespace Qtfy.Net.Numerics.Random.Samplers
                 for (j = 0; j < i; j++)
                 {
                     outputRowJ += j;
-                    sum = 0.0;
-                    for (k = 0; k < j; k++)
-                    {
-                        sum += outputRowI[k] * outputRowJ[k];
-                    }
-
-                    outputRowI[j] = 1.0 / outputRowJ[j] * (*inputRow - sum);
+                    outputRowI[j] = 1.0 / outputRowJ[j] * (*inputRow - Dot(outputRowI, outputRowI + j, outputRowJ));
                     ++inputRow;
                 }
 
                 outputRowJ += i;
-                sum = 0.0;
-                for (k = 0; k < i; k++)
-                {
-                    sum += outputRowI[k] * outputRowJ[k];
-                }
-
-                sum = Math.Sqrt(*inputRow - sum);
-                if (sum <= 0d || !double.IsFinite(sum))
+                sum = *inputRow - Dot(outputRowI, outputRowI + i, outputRowJ);
+                if (sum <= 0d)
                 {
                     throw new ArgumentException("expected positive definite matrix.");
                 }
 
-                outputRowI[i] = sum;
+                outputRowI[i] = Math.Sqrt(sum);
+            }
+
+            static double Dot(double* l, double* lend, double* r)
+            {
+                var sum = 0d;
+                while (l != lend)
+                {
+                    sum += *l * *r;
+                    ++l;
+                    ++r;
+                }
+
+                return sum;
             }
         }
 
-        /// <summary>
-        /// Checks the dimensions of a matrix and its indexation (it should start to count entries at zero).
-        /// </summary>
-        /// <param name="matrix">
-        /// The matrix to be checked.
-        /// </param>
-        /// <exception cref="ArgumentException">
-        /// If <paramref name="matrix"/> is not square,
-        /// of if <paramref name="matrix"/> is empty,
-        /// or if <paramref name="matrix"/> is not zero indexed.
-        /// </exception>
         private static void CheckDimensions(double[,] matrix)
         {
             if (matrix.GetLowerBound(0) != 0 || matrix.GetLowerBound(1) != 0)
@@ -152,32 +125,15 @@ namespace Qtfy.Net.Numerics.Random.Samplers
                 throw new ArgumentException("matrix must be zero indexed");
             }
 
-            var rows = matrix.GetLength(0);
-            var columns = matrix.GetLength(1);
-            if (rows != columns)
+            if (matrix.GetLength(0) != matrix.GetLength(1))
             {
                 throw new ArgumentException("matrix must be square");
             }
-
-            if (rows < 1)
-            {
-                throw new ArgumentException("matrix must not be empty.");
-            }
         }
 
-        /// <summary>
-        /// Checks the properties of a correlation matrix, i.e. whether it is a symmetric matrix and contains diagonal values equal to one.
-        /// </summary>
-        /// <param name="matrix">
-        /// The matrix to be checked.
-        /// </param>
-        /// <exception cref="ArgumentException">
-        /// if <paramref name="matrix"/> is not symmetric,
-        /// or if <paramref name="matrix"/> has diagonal elements that are not equal to one.
-        /// of if <paramref name="matrix"/> any non-diagonal elements are greater than one or less than minus one.
-        /// </exception>
-        private static void CheckCorrelationValues(double[,] matrix)
+        private static void AssertValidCorrelationMatrix(double[,] matrix)
         {
+            CheckDimensions(matrix);
             var rows = matrix.GetLength(0);
             for (var r = 0; r < rows; ++r)
             {
@@ -186,47 +142,36 @@ namespace Qtfy.Net.Numerics.Random.Samplers
                     var corr = matrix[r, c];
                     if (corr < -1d || corr > 1d || corr != matrix[c, r])
                     {
-                        throw new ArgumentException("Matrix must be symmetric and values must be in range [-1.0, 1.0].");
+                        throw new ArgumentException("matrix must be symmetric and only contain values in range [-1, 1]");
                     }
                 }
 
                 if (matrix[r, r] != 1d)
                 {
-                    throw new ArgumentException("Diagonal values must equal to 1.0.");
+                    throw new ArgumentException("diagonal entries must equal one");
                 }
             }
         }
 
-        /// <summary>
-        /// Checks the properties of a covariance matrix, i.e. whether
-        /// it is a symmetric matrix and contains positive diagonal values.
-        /// </summary>
-        /// <param name="matrix">
-        /// The matrix to be checked.
-        /// </param>
-        /// <exception cref="ArgumentException">
-        /// If <paramref name="matrix"/> is not symmetric.
-        /// or if any values in <paramref name="matrix"/> are less than zero,
-        /// or if any values in <paramref name="matrix"/> are double.NaN,
-        /// of if any values in <paramref name="matrix"/> infinite.
-        /// </exception>
-        private static void CheckCovarianceValues(double[,] matrix)
+        private static void AssertValidCovarianceMatrix(double[,] matrix)
         {
+            CheckDimensions(matrix);
             var rows = matrix.GetLength(0);
             for (var r = 0; r < rows; ++r)
             {
                 for (var c = 0; c < r; ++c)
                 {
-                    if (matrix[r, c] != matrix[c, r])
+                    var cov = matrix[r, c];
+                    if (!double.IsFinite(cov) || cov != matrix[c, r])
                     {
-                        throw new ArgumentException("Matrix must be symmetric.");
+                        throw new ArgumentException("matrix must be symmetric and have finite values.");
                     }
                 }
 
                 var variance = matrix[r, r];
                 if (!double.IsFinite(variance) || variance <= 0d)
                 {
-                    throw new ArgumentException("Diagonal values must be positive.");
+                    throw new ArgumentException("diagonal entries must be positive and finite.");
                 }
             }
         }
--- test/Qtfy.Net.Numerics.Tests/Random/Samplers/ImplTests.cs
+++ test/Qtfy.Net.Numerics.Tests/Random/Samplers/ImplTests.cs
@@ -13,16 +13,106 @@ namespace Qtfy.Net.Numerics.Tests.Random.Samplers
     public class ImplTests
     {
         [Test]
-        public void TestPackedCholeskyFactorCorrelationMatrix()
+        public void TestShapeAndSymmetry()
         {
-            var correlation = new[,]
+            static void CheckThrows(string message, double[,] input)
+            {
+                Assert.Throws<ArgumentException>(() => _ = PackedCholeskyFactorCorrelationMatrix(input), message);
+                Assert.Throws<ArgumentException>(() => _ = PackedCholeskyFactorCovarianceMatrix(input), message);
+            }
+
+            CheckThrows(
+                "not square",
+                new[,]
+                {
+                    { 0.5, 0.5 },
+                });
+
+            CheckThrows(
+                "not square",
+                new[,]
+                {
+                    { 0.5 },
+                    { 0.5 },
+                });
+
+            CheckThrows(
+                "zero indexing",
+                (double[,])Array.CreateInstance(typeof(double), new[] { 1, 1 }, new[] { 0, 1 }));
+
+            CheckThrows(
+                "zero indexing",
+                (double[,])Array.CreateInstance(typeof(double), new[] { 1, 1 }, new[] { 1, 0 }));
+
+            CheckThrows(
+                "not symmetric",
+                new[,]
+                {
+                    { 1.0, 0.8 },
+                    { 0.5, 1.0 },
+                });
+
+            CheckThrows(
+                "not symmetric due to nan",
+                new[,]
+                {
+                    { 1.0, double.NaN },
+                    { 0.5, 1.0 },
+                });
+
+            CheckThrows(
+                "symmetric nans",
+                new[,]
+                {
+                    { 1.0, double.NaN },
+                    { double.NaN, 1.0 },
+                });
+
+            CheckThrows(
+                "nan on diagonal",
+                new[,]
+                {
+                    { double.NaN, 0.5 },
+                    { 0.5, 1.0 },
+                });
+        }
+
+        [Test]
+        public void TestNonDefinite()
+        {
+            var negativeDefinite = new[,]
+            {
+                { 1.0, -1.1 },
+                { -1.1, 1.0 },
+            };
+
+            Assert.Throws<ArgumentException>(
+                () => _ = PackedCholeskyFactorCovarianceMatrix(negativeDefinite), "non definite");
+        }
+
+        [Test]
+        public void TestPositiveSemiDefinite()
+        {
+            var semiPositiveDefinite = new[,]
+            {
+                { 1.0, 1.0 },
+                { 1.0, 1.0 },
+            };
+
+            Assert.Throws<ArgumentException>(
+                () => _ = PackedCholeskyFactorCovarianceMatrix(semiPositiveDefinite), "semi positive definite");
+        }
+
+        [Test]
+        public void TestPackedCholeskyFactorMatrix()
+        {
+            var input = new[,]
             {
                 { 1.0, 0.5, 0.5 },
                 { 0.5, 1.0, 0.5 },
                 { 0.5, 0.5, 1.0 },
             };
 
-            var actual = PackedCholeskyFactorCorrelationMatrix(correlation);
             var expected = new[]
             {
                 1.0,
@@ -30,96 +120,107 @@ namespace Qtfy.Net.Numerics.Tests.Random.Samplers
                 0.5, 0.28867513459481292, 0.81649658092772603,
             };
 
-            Assert.AreEqual(actual, expected);
+            Assert.AreEqual(PackedCholeskyFactorCorrelationMatrix(input), expected);
+            Assert.AreEqual(PackedCholeskyFactorCovarianceMatrix(input), expected);
+
+            input = new[,]
+            {
+                { 1.0, 0.5 },
+                { 0.5, 1.0 },
+            };
+
+            expected = new[]
+            {
+                1.0,
+                0.5, 0.8660254037844386,
+            };
+
+            Assert.AreEqual(PackedCholeskyFactorCorrelationMatrix(input), expected);
+            Assert.AreEqual(PackedCholeskyFactorCovarianceMatrix(input), expected);
+
+            input = new[,]
+            {
+                { 1.0 },
+            };
+
+            expected = new[]
+            {
+                1.0,
+            };
+
+            Assert.AreEqual(PackedCholeskyFactorCorrelationMatrix(input), expected);
+            Assert.AreEqual(PackedCholeskyFactorCovarianceMatrix(input), expected);
         }
 
-        [Test]
-        public void TestInvalidCorrelationMatrix()
+        [TestCase(1.1)]
+        [TestCase(-1.1)]
+        [TestCase(0.9)]
+        [TestCase(-0.9)]
+        [TestCase(0d)]
+        [TestCase(double.PositiveInfinity)]
+        [TestCase(double.NegativeInfinity)]
+        [TestCase(double.NaN)]
+        public void TestInvalidCorrelationValuesOnDiagonal(double value)
+        {
+            Assert.Throws<ArgumentException>(() => _ = PackedCholeskyFactorCorrelationMatrix(new[,]
+            {
+                { value, 0.5 },
+                { 0.5, 1.0 },
+            }));
+
+            Assert.Throws<ArgumentException>(() => _ = PackedCholeskyFactorCorrelationMatrix(new[,]
+            {
+                { 1.0, 0.5 },
+                { 0.5, value },
+            }));
+        }
+
+        [TestCase(1.1)]
+        [TestCase(-1.1)]
+        [TestCase(double.PositiveInfinity)]
+        [TestCase(double.NegativeInfinity)]
+        [TestCase(double.NaN)]
+        public void TestInvalidCorrelationValues(double value)
+        {
+            Assert.Throws<ArgumentException>(() => _ = PackedCholeskyFactorCorrelationMatrix(new[,]
+            {
+                { 1.0, value },
+                { value, 1.0 },
+            }));
+        }
+
+        [TestCase(-1.0)]
+        [TestCase(0.0)]
+        [TestCase(double.NegativeInfinity)]
+        [TestCase(double.PositiveInfinity)]
+        [TestCase(double.NaN)]
+        public void TestInvalidCovarianceOnDiagonal(double value)
         {
             Assert.Throws<ArgumentException>(
-                () => _ = PackedCholeskyFactorCorrelationMatrix(new[,]
+                () => _ = PackedCholeskyFactorCovarianceMatrix(new[,]
                 {
-                    { 1.0, 0.8 },
-                    { 0.5, 1.0 },
+                    { 1.1, 2.0 },
+                    { 2.0, value },
                 }));
 
             Assert.Throws<ArgumentException>(
-                () => _ = PackedCholeskyFactorCorrelationMatrix(new[,]
+                () => _ = PackedCholeskyFactorCovarianceMatrix(new[,]
                 {
-                    { 1.0, -1.1 },
-                    { -1.1, 1.0 },
-                }));
-
-            Assert.Throws<ArgumentException>(
-                () => _ = PackedCholeskyFactorCorrelationMatrix(new[,]
-                {
-                    { 1.0, double.NaN },
-                    { double.NaN, 1.0 },
-                }));
-
-            Assert.Throws<ArgumentException>(
-                () => _ = PackedCholeskyFactorCorrelationMatrix(new[,]
-                {
-                    { 1.1, 0.5 },
-                    { 0.5, 1.0 },
+                    { value, 2.0 },
+                    { 2.0, 1.1 },
                 }));
         }
 
-        [Test]
-        public void TestInvalidCovarianceMatrix()
+        [TestCase(double.NaN)]
+        [TestCase(double.PositiveInfinity)]
+        [TestCase(double.NegativeInfinity)]
+        public void TestInvalidCovarianceValues(double value)
         {
             Assert.Throws<ArgumentException>(
                 () => _ = PackedCholeskyFactorCovarianceMatrix(new[,]
                 {
-                    { 1.0, 0.8 },
-                }));
-
-            Assert.Throws<ArgumentException>(
-                () => _ = PackedCholeskyFactorCovarianceMatrix(new double[0, 0] { }));
-
-            Assert.Throws<ArgumentException>(
-                () => _ = PackedCholeskyFactorCovarianceMatrix(new[,]
-                {
-                    { 1.0, 0.8 },
-                }));
-
-            Assert.Throws<ArgumentException>(
-                () => _ = PackedCholeskyFactorCovarianceMatrix(
-                    (double[,])Array.CreateInstance(typeof(double), new[] { 1, 1 }, new[] { 1, 1 })));
-
-            Assert.Throws<ArgumentException>(
-                () => _ = PackedCholeskyFactorCovarianceMatrix(new[,]
-                {
-                    { 1.0, 0.8 },
-                    { 0.5, 1.0 },
-                }));
-
-            Assert.Throws<ArgumentException>(
-                () => _ = PackedCholeskyFactorCovarianceMatrix(new[,]
-                {
-                    { 1.0, -1.1 },
-                    { -1.1, 1.0 },
-                }));
-
-            Assert.Throws<ArgumentException>(
-                () => _ = PackedCholeskyFactorCovarianceMatrix(new[,]
-                {
-                    { -1.0, 0.5 },
-                    { 0.5, 1.0 },
-                }));
-
-            Assert.Throws<ArgumentException>(
-                () => _ = PackedCholeskyFactorCovarianceMatrix(new[,]
-                {
-                    { 1.0, double.NaN },
-                    { double.NaN, 1.0 },
-                }));
-
-            Assert.Throws<ArgumentException>(
-                () => _ = PackedCholeskyFactorCovarianceMatrix(new[,]
-                {
-                    { double.NaN, 0.5 },
-                    { 0.5, 1.0 },
+                    { 1.1, value },
+                    { value, 1.1 },
                 }));
         }
     }
